cd67fba62199fa6195c02233a99ebf45cc7dc131
vedikashah_07 <iamvshah2004@gmail.com>
2024-07-24 10:45:41 +0530

    first commit

---
+++ js aajse phirse/script.js
@@ -0,0 +1,17 @@
+alert("hello world");
+console.log("code is running...")
+var a= prompt("enter")
+var isTrue = confirm("are you sure??")
+if (isTrue)
+    
+    {
+        console.log("computer is blasting")
+
+    }
+
+    else
+    {
+
+        console.log("computer isnt blasting")
+        
+    }
---
+++ js aajse phirse/script2.js
@@ -0,0 +1,10 @@
+console.log("hey this is tuto 55")
+var a= 5;
+var b= 6;
+var _a="harry";
+// var 55a="vedika ";
+//nit allowed
+console.log(a+b+8);
+console.log(typeof a, typeof b,typeof c);
+const a1=6;
+a1= a1+1;   //not allowed because a1 is constant
